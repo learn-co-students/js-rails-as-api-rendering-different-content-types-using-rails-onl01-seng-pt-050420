7997244a9310a1c93b4a77bbfebabe4e7ba491db
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-11-16 16:29:36 -0500

    Done.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,6 @@
-class Bird < ApplicationRecord
-end
+class BirdsController < ApplicationController
+    def index
+      @birds = Bird.all
+      render plain: "Hello #{@birds[3].name}"
+    end
+  end
